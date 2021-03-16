376335da604bb9acb4d1de2845cefaac8f244a42
Toan Nguyen (Daniel) <khanhtoanfrozen@gmail.com>
2021-03-16 15:44:50 +0700

    Initial Commit

---
+++ BaseProjectSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseProjectSwift
+//
+//  Created by Nguyen Toan on 3/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
